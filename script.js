6fbc231fc76ea939011bc66b4f1d553b8d98b9ea
Jacob Duden <Jlavaball@gmail.com>
2021-03-24 17:59:21 -0500

    corrected timer functionality

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ var currentScoreEl = document.querySelector("#score");
 var timeRemaining = document.querySelector("#timer");
 var answerButton = document.querySelector("#answerBtns");
 var answersEl = document.getElementById("answers");
-var highScoreEl = document.getElementById("high-scores")
+var highScoreEl = document.getElementById("high-scores");
+var feedBack = document.getElementById("feed-back");
 
 var timerCount = 90;
 var score = 0;
 var questionIndex = 0;
+
 
 // start quiz game
 var startQuiz = function () {
@@ -33,8 +35,9 @@ var time = function () {
         // clearInterval(timerCount);
         if (timerCount <= 0) {
             clearInterval(timer);
-            
-            
+            endGame();
+            feedBack.style.display = "block"
+            feedBack.textContent = "Your time ran out!"
             
         }
     }, 1000);
@@ -46,7 +49,7 @@ var replaceQ = function () {
     var currentQuestion = quizQuestions[questionIndex];
     var titleEl = document.getElementById("mainQuestion");
     titleEl.textContent = currentQuestion.question;
-
+    currentScoreEl.textContent = score + "/4";
     answersEl.textContent = "";
     currentQuestion.answers.forEach(function (currentAnswer) {
         var choices = document.createElement("button");
@@ -55,15 +58,21 @@ var replaceQ = function () {
         if (timerCount <= 0) {
             return (replaceQ);
         }
-
+        if (questionIndex >= quizQuestions.length) {
+            endGame();
+        }
+        
         choices.addEventListener("click", function () {
             if (currentAnswer === quizQuestions[questionIndex].correctAnswer) {
                 questionIndex++;
                 score++;
                 replaceQ();
+                feedBack.style.display = "none"
             }
             else {
                 timerCount -= 15;
+                feedBack.textContent = "Incorrect";
+                feedBack.style.display = "block";
             }
         })
     });
@@ -80,6 +89,10 @@ var endGame = function () {
         mainScreenEl.style.display = "block";
         highScoreEl.style.display = "none";
         restartBtn.style.display = "none";
+        feedBack.style.display = "none";
+        timerCount = 90;
+        score = 0;
+        questionIndex = 0;
         return (endGame);
 
 
